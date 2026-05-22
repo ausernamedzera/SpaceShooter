132982e5460dbc396def2084011d32b7e348114f
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-22 23:57:01 +0300

    game_window

--- objects/game.py
+++ objects/game.py
@@ -1,2 +1,10 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Space Shooter"
+
 class Game:
-    pass
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set.background_color(arcade.color.BLACK)
