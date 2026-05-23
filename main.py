6252e10f5fc98a03209c5b15573f50b67f1565be
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-23 22:22:12 +0300

    constants_imported

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#from objects.player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from objects.player import Player
 from objects.enemies import Enemies
 from objects.game import Game
 import arcade
