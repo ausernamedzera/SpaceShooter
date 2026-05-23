efb0a8599ebd967cc0dbf4a3eceeed97de08fdc0
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-23 22:05:05 +0300

    creating_game_screen_added_to_main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from objects.player import Player
+#from objects.player import Player
 from objects.enemies import Enemies
 from objects.game import Game
+import arcade
+
+def main():
+    game_window = Game()
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
