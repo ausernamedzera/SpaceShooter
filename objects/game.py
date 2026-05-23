fad777fc406b5291a275bdc8cf0cace0fb66ed01
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-23 22:21:50 +0300

    created_a_player_object

--- objects/game.py
+++ objects/game.py
@@ -1,13 +1,13 @@
 import arcade #everynew window, we need to install again using "pip install arcade --no-deps"
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Space Shooter"
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
+from objects.player import Player
 
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.player = Player()
         arcade.set_background_color(arcade.color.GRAY_BLUE)
 
     def on_draw(self):
         self.clear()
+        self.player.on_draw()
