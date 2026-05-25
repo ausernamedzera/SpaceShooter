144bae14cdccc80f2b74c416c956c1389cfc30e3
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-26 00:00:43 +0300

    draw_and_update

--- objects/enemy.py
+++ objects/enemy.py
@@ -2,10 +2,14 @@ import arcade
 import random
 
 class Enemy:
-    def __init__(self, x, y, spawn_timer):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.spawn_timer = spawn_timer
+        self.spawn_timer = 0
 
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, 15, arcade.color.RAW_UMBER)
+
+    def update(self, delta_time):
+        self.spawn_timer += delta_time
+
