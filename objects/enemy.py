d920c3b0e2b49bc73b607b6362d9cbc979a0a74d
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-26 00:48:39 +0300

    bug_fix_v2

--- objects/enemy.py
+++ objects/enemy.py
@@ -5,11 +5,10 @@ class Enemy:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speed = 0
+        self.speed = 3
 
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, 15, arcade.color.RAW_UMBER)
 
-    def update(self, delta_time):
-        self.speed += delta_time
-
+    def update(self):
+        self.y -= self.speed
