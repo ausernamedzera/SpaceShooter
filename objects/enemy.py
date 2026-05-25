d2ff782396c5b5e8999b76a1665008037556ada2
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-25 23:44:40 +0300

    class_enemy

--- objects/enemy.py
+++ objects/enemy.py
@@ -2,5 +2,7 @@ import arcade
 import random
 
 class Enemy:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, spawn_timer):
+        self.x = x
+        self.y = y
+        self.spawn_timer = spawn_timer
