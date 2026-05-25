7cc42289d458b4c99cb5f00e8b225d1350275bfc
ausernamedzera <hellodarkimthelight@hotmail.com>
2026-05-26 00:39:43 +0300

    speed

--- objects/enemy.py
+++ objects/enemy.py
@@ -5,11 +5,11 @@ class Enemy:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.spawn_timer = 0
+        self.speed = 0
 
     def draw(self):
         arcade.draw_circle_filled(self.x, self.y, 15, arcade.color.RAW_UMBER)
 
     def update(self, delta_time):
-        self.spawn_timer += delta_time
+        self.speed += delta_time
 
